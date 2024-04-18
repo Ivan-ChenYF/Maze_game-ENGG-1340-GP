88cbc7585333bb26d488666b520fea189fe62b6a
Ivan-ChenYF <103233039+Ivan-ChenYF@users.noreply.github.com>
2024-04-19 05:20:27 +0800

    Update random_map.cpp

--- random_map.cpp
+++ random_map.cpp
@@ -3,6 +3,7 @@
 #include <stack>
 #include <cstdlib>
 #include <ctime>
+#include <ncurses.h>
 using namespace std;
 
 //每一个点的信息包含坐标
@@ -12,21 +13,13 @@ struct cell{
 };
 
 void init(cell* now,int initX, int initY);
-void printMaze(cell** &maze, int width, int height);
 void generateMaze(cell** &maze, int width,int height,int start_x,int start_y);
-
+void printMaze(cell** maze, int width, int height, int play_posit_x,int play_posit_y);
 int main(){
     int width;
     int height;
     int start_x,start_y;
-    cout<<"Width: ";
-    cin>>width;
-    cout<<"Height: ";
-    cin>>height;
-    cout<<"start_x: ";
-    cin>>start_x;
-    cout<<"start_y: ";
-    cin>>start_y;
+    cin>>width>>height>>start_x>>start_y;
     cell** maze = new cell*[width]; //使用动态数组创建maze
     for (int i = 0; i < width; ++i) {
         maze[i] = new cell [height];
@@ -37,6 +30,7 @@ int main(){
         }
     }
     generateMaze(maze, width,height,start_x,start_y);
+    printMaze(maze, width,height,start_x,start_y);
 }
 
 void init(cell *now,int initX, int initY) {
@@ -115,4 +109,70 @@ void generateMaze(cell** &maze, int width,int height,int start_x,int start_y){
         }
     }
     
+}
+
+
+void printMaze(cell** maze, int width, int height, int player_x,int player_y) {
+    initscr();          // Start ncurses mode
+    cbreak();           // Disable line buffering
+    noecho();           // Don't echo user input
+    keypad(stdscr, TRUE); // Enable function keys
+    curs_set(0);        // Hide the cursor
+    start_color();          // 启动颜色功能
+
+    init_pair(1, COLOR_RED, COLOR_WHITE); // 定义颜色对，红色前景，白色背景
+
+    bkgd(COLOR_PAIR(1));    // 设置默认背景颜色为白色
+    attron(COLOR_PAIR(1));  // 使用红色前景和白色背景
+
+ 
+    mvaddch(2 * player_y + 1, 3 * player_x + 1, '@');
+
+    // The corner and edges need to be handled for the entire grid
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            int x = 3 * i;
+            int y = 2 * j;
+
+            // Display the walls of the cell
+            mvaddch(y, x, '+'); // Corner of the cell
+            if (maze[i][j].top_w || j == 0) {
+                mvaddch(y, x + 1, '-'); // Top wall
+                mvaddch(y, x + 2, '-');
+            }
+            if (maze[i][j].left_w || i == 0) {
+                mvaddch(y + 1, x, '|'); // Left wall
+            }
+            if (maze[i][j].right_w || i == width - 1) {
+                mvaddch(y + 1, x + 3, '|'); // Right wall
+            }
+            if (maze[i][j].down_w || j == height - 1) {
+                mvaddch(y + 2, x, '+'); // Bottom corner
+                mvaddch(y + 2, x + 1, '-'); // Bottom wall
+                mvaddch(y + 2, x + 2, '-');
+            }
+        }
+    }
+
+    // Correctly handling the last row and the last column
+    // Add corners and vertical lines on the right edge of the maze
+    for (int j = 0; j <= height; j++) {
+        mvaddch(2 * j, 3 * width, '+'); // Add corners
+        if (j < height) {
+            mvaddch(2 * j + 1, 3 * width, '|'); // Add vertical lines
+        }
+    }
+
+    // Add corners and horizontal lines on the bottom edge of the maze
+    for (int i = 0; i <= width; i++) {
+        mvaddch(2 * height, 3 * i, '+'); // Add corners
+        if (i < width) {
+            mvaddch(2 * height, 3 * i + 1, '-'); // Add horizontal lines
+            mvaddch(2 * height, 3 * i + 2, '-');
+        }
+    }
+    attroff(COLOR_PAIR(1)); // 关闭颜色属性
+    refresh();         // Refresh the screen to show the maze
+    getch();           // Wait for user input
+    endwin();          // End ncurses mode
 }
