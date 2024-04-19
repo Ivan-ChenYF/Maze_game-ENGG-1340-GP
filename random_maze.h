e8373ff3a1c787a0dcbc6e596907e9803e543845
Ivan-ChenYF <103233039+Ivan-ChenYF@users.noreply.github.com>
2024-04-19 19:24:41 +0800

    Create random_maze.h

---
+++ random_maze.h
@@ -0,0 +1,16 @@
+#ifndef RANDOM_MAZE_H
+#define RANDOM_MAZE_H
+
+#include <vector>
+
+struct cell {
+    int x, y;
+    bool visited, revealed;
+    bool top_w, left_w, right_w, down_w;
+};
+
+void init(cell* now, int initX, int initY);
+void generateMaze(cell*** maze, int width, int height, int start_x, int start_y);
+void printMaze(cell** maze, int width, int height, int player_x, int player_y);
+
+#endif
