9730e1b649d195688f3ebbdbe36be5d3f78243d0
Ivan-ChenYF <103233039+Ivan-ChenYF@users.noreply.github.com>
2024-04-19 20:12:43 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,14 @@ int main(){
     int width;
     int height;
     int start_x,start_y;
-    cin>>width>>height>>start_x>>start_y;
+    cout<<"Width: ";
+    cin>>width;
+    cout<<"Height: ";
+    cin>>height;
+    cout<<"start_x: ";
+    cin>>start_x;
+    cout<<"start_y: ";
+    cin>>start_y;
     cell** maze = new cell*[width]; //使用动态数组创建maze
     for (int i = 0; i < width; ++i) {
         maze[i] = new cell [height];
