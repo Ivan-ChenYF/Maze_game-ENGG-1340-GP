29ab64e648262c8a96ab10aaed1ab7ce33dec93e
BYLinMou <159423310+BYLinMou@users.noreply.github.com>
2024-04-20 11:34:37 +0800

    Update main.cpp
    ADD USER INPUT PAGE

--- main.cpp
+++ main.cpp
@@ -5,13 +5,40 @@
 using namespace std;
 
 int main(){
+
     setlocale(LC_ALL, "");
+    
+    // LOADING PAGE
     initscr();
     PrintFromFile("ASCII - Enigma_Maze.txt");
     getch();
     clear();
     endwin();
 
+    // INPUT USERNAME
+    initscr();
+    PrintFromFile("ASCII - Create_User.txt");
+    string username;
+    char ch;
+    while ((ch = getch()) != '\n') {
+        if (ch == KEY_BACKSPACE || ch == 127) {
+            if (!username.empty()) {
+                username.pop_back();
+                move(getcury(stdscr), getcurx(stdscr) - 1);
+                mvdelch(getcury(stdscr), getcurx(stdscr));
+                refresh();
+            }
+        } else {
+            username.push_back(ch);
+            refresh();
+        }
+    }
+    clear();
+    endwin();
+
+    cout << "Your user name is " << username << endl;
+
+    // RANDOM MAP GENERATION
     int width;
     int height;
     int start_x,start_y;
@@ -41,5 +68,3 @@ int main(){
     delete[] maze;
     return 0;
 }
-
-
