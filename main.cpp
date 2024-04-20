a14d0bb5359d9f3b07a97544e5eee4ba1c45e56e
Tony Lok <159618278+Tony70972@users.noreply.github.com>
2024-04-20 17:19:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 int main(){
-
+    gameState game; // Initialize gameState
+    
     setlocale(LC_ALL, "");
     
     // LOADING PAGE
@@ -19,25 +20,24 @@ int main(){
     // INPUT USERNAME
     initscr();
     PrintFromFile("ASCII - Create_User.txt");
-    string username;
     char ch;
     while ((ch = getch()) != '\n') {
         if (ch == KEY_BACKSPACE || ch == 127) {
-            if (!username.empty()) {
-                username.pop_back();
+            if (!game.player_name.empty()) {
+                game.player_name.pop_back();
                 move(getcury(stdscr), getcurx(stdscr) - 1);
                 mvdelch(getcury(stdscr), getcurx(stdscr));
                 refresh();
             }
         } else {
-            username.push_back(ch);
+            game.player_name.push_back(ch);
             refresh();
         }
     }
     clear();
     endwin();
 
-    cout << "Your user name is " << username << endl;
+    cout << "Your user name is " << game.player_name << endl;
 
     // RANDOM MAP GENERATION
     int width;
