4c6e3b90520bbe75860eab631d7795f95d5efd48
Tim Wu <149659039+Tim0308@users.noreply.github.com>
2024-04-22 20:02:07 +0800

    included the key_up, key_donw....

--- player_control.cpp
+++ player_control.cpp
@@ -3,107 +3,110 @@
 #include <stack>
 #include <termios.h>
 #include <unistd.h>
-#include "play_control.h"
+#include "player_control.h"
 #include "random_maze.h"
 #include <ncurses.h>
 
 using namespace std;
 
-char getSingleKeyPress()
+void player_movement(cell **maze, int width, int height, int &playerX, int &playerY, int input)
 {
-    termios oldSettings, newSettings;
-    tcgetattr(STDIN_FILENO, &oldSettings);
-    newSettings = oldSettings;
-    newSettings.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);
-
-    char key = getchar();
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldSettings);
-    return key;
-}
-
-
-void player_movement(cell **maze, int width, int height, int start_x, int start_y)
-{
-    int playerY = start_y, playerX = start_x;       // Initial player position
-    mvaddch(2 * playerY + 1, 3 * playerX + 1, '@'); // Draw player at initial position
+    // Draw player at initial position
 
     while (true)
     {
-        char input = getSingleKeyPress();
-        // int playerY = start_y, playerX = start_x;
+        // char input = getSingleKeyPress();
+        //  int playerY = start_y, playerX = start_x;
         switch (input)
         {
-        case 'w':
-            if (playerY > 0 && !maze[newY-1][newX].down_w)
+        case 119:
+            if (playerY > 0 && !maze[playerY - 1][playerX].down_w)
             {
-                mvaddch(2 * playerY + 1, 3 * playerX + 1, ' '); // Clear current position
                 playerY--;
-                mvaddch(2 * playerY + 1, 3 * playerX + 1, '@'); // Draw player at new position
-                start_y = playerY;
             }
             else
             {
                 mvprintw(0, height + 2, "cant move");
-                printMaze(maze, width, height, start_x, start_y);
                 break;
             }
             break;
-        case 'a':
-            if (playerX > 0 && !maze[newY][newX-1].right_w)
+        case 97:
+            if (playerX > 0 && !maze[playerY][playerX - 1].right_w)
             {
-                mvaddch(2 * playerY + 1, 3 * playerX + 1, ' '); // Clear current position
                 playerX--;
-                mvaddch(2 * playerY + 1, 3 * playerX + 1, '@'); // Draw player at new position
-                start_x = playerX;
             }
             else
             {
                 mvprintw(0, height + 2, "cant move");
-                printMaze(maze, width, height, start_x, start_y);
                 break;
             }
             break;
-        case 's':
-            if (playerY < height - 1 !!maze[newY+1][newX].down_w)
+        case 115:
+            if (playerY < height - 1 && !maze[playerY + 1][playerX].down_w)
             {
-                mvaddch(2 * playerY + 1, 3 * playerX + 1, ' '); // Clear current position
                 playerY++;
-                mvaddch(2 * playerY + 1, 3 * playerX + 1, '@'); // Draw player at new position
-                start_y = playerY;
             }
             else
             {
                 mvprintw(0, height + 2, "cant move");
-                printMaze(maze, width, height, start_x, start_y);
                 break;
             }
             break;
-        case 'd':
-            if (playerX < width - 1 && !maze[newY][newX+1].right_w)
+        case 100:
+            if (playerX < width - 1 && !maze[playerY][playerX + 1].right_w)
             {
-                mvaddch(2 * playerY + 1, 3 * playerX + 1, ' '); // Clear current position
                 playerX++;
-                mvaddch(2 * playerY + 1, 3 * playerX + 1, '@'); // Draw player at new position
-                start_x = playerX;
+            }
+
+        case KEY_UP:
+            if (playerY > 0 && !maze[playerY - 1][playerX].down_w)
+            {
+                playerY--;
             }
             else
             {
                 mvprintw(0, height + 2, "cant move");
-                printMaze(maze, width, height, start_x, start_y);
+                break;
+            }
+            break;
+        case KEY_LEFT:
+            if (playerX > 0 && !maze[playerY][playerX - 1].right_w)
+            {
+                playerX--;
+            }
+            else
+            {
+                mvprintw(0, height + 2, "cant move");
+                break;
+            }
+            break;
+        case KEY_DOWN:
+            if (playerY < height - 1 && !maze[playerY + 1][playerX].down_w)
+            {
+                playerY++;
+            }
+            else
+            {
+                mvprintw(0, height + 2, "cant move");
+                break;
+            }
+            break;
+        case KEY_RIGHT:
+            if (playerX < width - 1 && !maze[playerY][playerX + 1].right_w)
+            {
+                playerX++;
+            }
+            else
+            {
+                mvprintw(0, height + 2, "cant move");
                 // refresh();
             }
+
             break;
-        case 'q':
-            // mvprintw( height +2, 0,  "Quitting th)" << endl;
-            return; // Exit the function and stop the game
         default:
-            // smvprintw(0, 0, "Invalid input. Please enter w, a, s, or d.");
             break;
         }
 
         refresh(); // Refresh the screen to show the changes
-        // printMaze(maze, width, height, start_x, start_y);
     }
 }
