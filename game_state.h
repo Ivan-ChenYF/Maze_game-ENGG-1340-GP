bafe52f7a2cde6f2ba03cbea4711a9675c6ce22f
Tony Lok <159618278+Tony70972@users.noreply.github.com>
2024-04-22 16:03:27 +0800

    Update game_state.h

--- game_state.h
+++ game_state.h
@@ -2,18 +2,27 @@
 #define GAME_STATE_H
 
 #include <string>
+#include <vector>
 #include <fstream>
 #include "random_maze.h"
 using namespace std;
 
-struct gameState {
+struct Item {
+    string name;
+    int quantity;
+};
+using Inventory = vector<Item>;
+
+struct GameState {
     cell** maze;
     string player_name;
     int width, height;
     int player_x, player_y;
+    Inventory inventory;
 };
 
-void saveGame(const gameState& gameState, const string& filename);
-void loadGame(const gameState& gameState, const string& filename);
+void initializeNewGame(GameState& gameState);
+void saveGame(const GameState& gameState, const string& filename);
+void loadGame(const GameState& gameState, const string& filename);
 
 #endif
