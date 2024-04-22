2ca9a7e54ea18e7e441d1359fd31c0bbafa6908d
Tim Wu <149659039+Tim0308@users.noreply.github.com>
2024-04-22 21:30:53 +0800

    Update player_control.h

--- player_control.h
+++ player_control.h
@@ -4,7 +4,7 @@
 #include "random_maze.h" // Include the header file where the cell struct is defined
 
 
-void player_movement(cell **maze, int width, int height, int player_x, int player_y, int input);
+void player_movement(cell **&maze, int width, int height, int &player_x, int &player_y, int input);
 
 #endif
 
