13222b997a46f928ff30e028befc8d930319da3d
Tim Wu <149659039+Tim0308@users.noreply.github.com>
2024-04-22 20:53:49 +0800

    Update player_control.h => keep the player_movement only

--- player_control.h
+++ player_control.h
@@ -3,10 +3,8 @@
 
 #include "random_maze.h" // Include the header file where the cell struct is defined
 
-void clearScreen();
-char getSingleKeyPress();
-bool isValidMove(cell **maze, int newY, int newX, int height, int width);
-void player_movement(cell **maze, int width, int height, int player_x, int player_y);
+
+void player_movement(cell **maze, int width, int height, int player_x, int player_y, int input);
 
 #endif
 
