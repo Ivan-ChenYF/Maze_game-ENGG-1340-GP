b5f2090e2b171bdec2dd3c72a00692a5a4665b6c
Tim Wu <149659039+Tim0308@users.noreply.github.com>
2024-04-24 01:22:56 +0800

    Update print_maze.cpp

--- print_maze.cpp
+++ print_maze.cpp
@@ -27,7 +27,7 @@ void printMaze(cell** maze, int width, int height, int player_x,int player_y,int
    
     mvprintw(2 * end_y + 1, 4 * end_x + 1, "📍");
 
-    mvprintw(2*height+2, 0, "Time left: %.0f seconds", elapsed);
+    mvprintw(2*height+2, 0, "Time remaining: %.0f seconds", elapsed);
     mvaddch(2 * player_y + 1, 4 * player_x + 2, '@');
     mvprintw(2 * height + 2, 4 * width + 1, "Bomb: %d", bomb);
     // The corner and edges need to be handled for the entire grid
