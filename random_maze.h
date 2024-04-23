82708899dca7c7ea4614a296a833c8ab392580ce
Ivan-ChenYF <103233039+Ivan-ChenYF@users.noreply.github.com>
2024-04-23 14:35:37 +0800

    Update random_maze.h

--- random_maze.h
+++ random_maze.h
@@ -4,7 +4,6 @@
 #include <vector>
 #include <string>
 using namespace std;
-
 struct cell {
     int x, y;
     bool visited, revealed;
@@ -15,6 +14,7 @@ void init(cell* now, int initX, int initY);
 void generateMaze(cell** &maze, int width, int height, int start_x, int start_y,int &end_x,int &end_y);
 void printMaze(cell** maze, int width, int height, int player_x, int player_y,int end_x,int end_y);
 void PrintFromFile(std::string filename);
-int choiceUI(string choice);
+int choiceUImode();
+int choiceUIdiff();
 
 #endif
