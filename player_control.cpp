2f1c634ecc5d4810e73edae1c00d7f279f7c033a
Tim Wu <149659039+Tim0308@users.noreply.github.com>
2024-04-24 12:59:03 +0800

    Update player_control.cpp => updated the bomb

--- player_control.cpp
+++ player_control.cpp
@@ -13,9 +13,9 @@ void player_movement(cell **&maze, int width, int height, int &playerX, int &pla
 
     // char input = getSingleKeyPress();
     //  int playerY = start_y, playerX = start_x;
-    start_color();          // 启动颜色功能
+    start_color();                        // 启动颜色功能
     init_pair(1, COLOR_RED, COLOR_WHITE); // 定义颜色对，红色前景，白色背景
-    attron(COLOR_PAIR(1));  // 使用红色前景和白色背景
+    attron(COLOR_PAIR(1));                // 使用红色前景和白色背景
     switch (input)
     {
     case 119:
@@ -25,7 +25,7 @@ void player_movement(cell **&maze, int width, int height, int &playerX, int &pla
         }
         else
         {
-            mvprintw(2 * height + 1, 4 * width + 1,"Invalid Move");
+            mvprintw(2 * height + 1, 4 * width + 1, "Invalid Move");
             break;
         }
         break;
@@ -36,7 +36,7 @@ void player_movement(cell **&maze, int width, int height, int &playerX, int &pla
         }
         else
         {
-            mvprintw(2 * height + 1, 4 * width + 1,"Invalid Move");
+            mvprintw(2 * height + 1, 4 * width + 1, "Invalid Move");
             break;
         }
         break;
@@ -47,7 +47,7 @@ void player_movement(cell **&maze, int width, int height, int &playerX, int &pla
         }
         else
         {
-            mvprintw(2 * height + 1, 4 * width + 1,"Invalid Move");
+            mvprintw(2 * height + 1, 4 * width + 1, "Invalid Move");
             break;
         }
         break;
@@ -58,10 +58,10 @@ void player_movement(cell **&maze, int width, int height, int &playerX, int &pla
         }
         else
         {
-            mvprintw(2 * height + 1, 4 * width + 1,"Invalid Move");
+            mvprintw(2 * height + 1, 4 * width + 1, "Invalid Move");
             break;
         }
-            break;
+        break;
 
     case KEY_UP:
         if (playerY > 0 && !maze[playerX][playerY].top_w)
@@ -70,7 +70,7 @@ void player_movement(cell **&maze, int width, int height, int &playerX, int &pla
         }
         else
         {
-            mvprintw(2 * height + 1, 4 * width + 1,"Invalid Move");
+            mvprintw(2 * height + 1, 4 * width + 1, "Invalid Move");
             break;
         }
         break;
@@ -81,7 +81,7 @@ void player_movement(cell **&maze, int width, int height, int &playerX, int &pla
         }
         else
         {
-            mvprintw(2 * height + 1, 4 * width + 1,"Invalid Move");
+            mvprintw(2 * height + 1, 4 * width + 1, "Invalid Move");
             break;
         }
         break;
@@ -92,7 +92,7 @@ void player_movement(cell **&maze, int width, int height, int &playerX, int &pla
         }
         else
         {
-            mvprintw(2 * height + 1, 4 * width + 1,"Invalid Move");
+            mvprintw(2 * height + 1, 4 * width + 1, "Invalid Move");
             break;
         }
         break;
@@ -103,66 +103,72 @@ void player_movement(cell **&maze, int width, int height, int &playerX, int &pla
         }
         else
         {
-            mvprintw(2 * height + 1, 4 * width + 1,"Invalid Move");
+            mvprintw(2 * height + 1, 4 * width + 1, "Invalid Move");
             // refresh();
             break;
         }
-            break;
+        break;
     case 98: // 'b' for bomb
 
-            time_t start_time = time(nullptr);
-            time_t current_time;
-            int ch;
-            double elapsed;
-  
+        time_t start_time = time(nullptr);
+        time_t current_time;
+        int ch;
+        double elapsed;
 
-            while (true) {
-                current_time = time(nullptr);
-                elapsed = difftime(current_time, start_time);
-                mvprintw(2 * height + 1, 0, "You got a bomb, press arrow key to choose wall to destroy");
-                refresh();
+        while (bomb != 0)
+        {
+            current_time = time(nullptr);
+            elapsed = difftime(current_time, start_time);
+            mvprintw(2 * height + 1, 0, "You got a bomb, press arrow key to choose wall to destroy");
+            refresh();
 
-                if (elapsed > 2) {  // 如果超过2秒，退出循环
-                    break;
-                }
-
-                ch = getch();  // 获取键盘输入
-                switch (ch) {
-                    case ERR:  // 如果没有输入，继续循环
-                        continue;
-                    case KEY_UP:
-                        if (playerY > 0 && maze[playerX][playerY].top_w) {
-                            bomb--;
-                            maze[playerX][playerY].top_w = false;
-                            maze[playerX][playerY - 1].down_w = false;
-                        }
-                        break;
-                    case KEY_LEFT:
-                        if (playerX > 0 && maze[playerX][playerY].left_w) {
-                            bomb--;
-                            maze[playerX][playerY].left_w = false;
-                            maze[playerX - 1][playerY].right_w = false;
-                        }
-                        break;
-                    case KEY_DOWN:
-                        if (playerY < height - 1 && maze[playerX][playerY].down_w) {
-                            bomb--;
-                            maze[playerX][playerY].down_w = false;
-                            maze[playerX][playerY + 1].top_w = false;
-                        }
-                        break;
-                    case KEY_RIGHT:
-                        if (playerX < width - 1 && maze[playerX][playerY].right_w) {
-                            bomb--;
-                            maze[playerX][playerY].right_w = false;
-                            maze[playerX + 1][playerY].left_w = false;
-                        }
-                        break;
-                }
-                break;  // 一旦接收到有效输入即退出循环
+            if (elapsed > 2.5)
+            { // 如果超过2秒，退出循环
+                break;
             }
 
+            ch = getch(); // 获取键盘输入
+            switch (ch)
+            {
+            case ERR: // 如果没有输入，继续循环
+                continue;
+            case KEY_UP:
+                if (playerY > 0 && maze[playerX][playerY].top_w)
+                {
+                    bomb--;
+                    maze[playerX][playerY].top_w = false;
+                    maze[playerX][playerY - 1].down_w = false;
+                }
+                break;
+            case KEY_LEFT:
+                if (playerX > 0 && maze[playerX][playerY].left_w)
+                {
+                    bomb--;
+                    maze[playerX][playerY].left_w = false;
+                    maze[playerX - 1][playerY].right_w = false;
+                }
+                break;
+            case KEY_DOWN:
+                if (playerY < height - 1 && maze[playerX][playerY].down_w)
+                {
+                    bomb--;
+                    maze[playerX][playerY].down_w = false;
+                    maze[playerX][playerY + 1].top_w = false;
+                }
+                break;
+            case KEY_RIGHT:
+                if (playerX < width - 1 && maze[playerX][playerY].right_w)
+                {
+                    bomb--;
+                    maze[playerX][playerY].right_w = false;
+                    maze[playerX + 1][playerY].left_w = false;
+                }
+                break;
+            }
+            break; // 一旦接收到有效输入即退出循环
+        }
     }
     attroff(COLOR_PAIR(1)); // 关闭颜色属性
-    refresh(); // Refresh the screen to show the changes
+    refresh();              // Refresh the screen to show the changes
 }
+
