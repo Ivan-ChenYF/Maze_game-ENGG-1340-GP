16a9b3e8550c7f5f4c9848ec0c7e641f072d9fd6
BYLinMou <159423310+BYLinMou@users.noreply.github.com>
2024-04-25 23:25:44 +0800

    Update main_menu.cpp

--- main_menu.cpp
+++ main_menu.cpp
@@ -5,6 +5,7 @@
 #include "user_interface.h"
 using namespace std;
 
+
 int choiceUI(OptionType optionType, string username) {
     
     const Option modeOptions[] = {
@@ -31,32 +32,33 @@ int choiceUI(OptionType optionType, string username) {
         options = difficultyOptions;
         numOptions = sizeof(difficultyOptions) / sizeof(difficultyOptions[0]);
     } else {
-        return -1; // 无效的选项类型
+        return -1; // INVALIE OPTION TYPE
     }
 
-    // 初始化ncurses
+    // INITIALIZE NCURSES
     initscr();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
     curs_set(0);
 
-    // 获取屏幕尺寸
+    // GET SCREEN SIZE
     int maxRows, maxCols;
     getmaxyx(stdscr, maxRows, maxCols);
 
     int currentOption = 0;
 
     while (true) {
-        // 清空屏幕
+
+        // PRINT MAIN MENU
         clear();
         PrintFromFile("ASCII - Main_Menu.txt");
         mvprintw(9, 0, (" Hi, " + username + ".").c_str());
 
-        // 打印选项
+        // PRINT CHOICES
         for (int i = 0; i < numOptions; i++) {
             if (i == currentOption) {
-                // 设置选中项的颜色为白底黑字
+                // SLECTED ITEM -> DIFFERENT COLOR
                 attron(A_REVERSE);
                 attron(COLOR_PAIR(1));
             }
@@ -66,10 +68,9 @@ int choiceUI(OptionType optionType, string username) {
             attroff(COLOR_PAIR(1));
         }
 
-        // 刷新屏幕
         refresh();
 
-        // 获取用户输入
+        // USER INPUT
         int input = getch();
         switch (input) {
             case KEY_UP:
@@ -79,48 +80,39 @@ int choiceUI(OptionType optionType, string username) {
                 currentOption = (currentOption + 1) % numOptions;
                 break;
             case '\n':
-                // 用户按下Enter键，选择当前选项
+                // SELECTION (ENTER)
                 clear();
                 mvprintw(maxRows/2 - 1, maxCols/2 - (strlen(options[currentOption].text) + 10)/2, "Selected: %s", options[currentOption].text);
                 refresh();
-                usleep(550000);
+                usleep(450000);
                 mvprintw(maxRows/2, maxCols/2 - strlen("Loading...")/2, "Loading...");
                 refresh();
-                usleep(250000);
+                usleep(350000);
                 clear();
                 refresh();
                 return options[currentOption].value;
         }
     }
 
-    // 清理ncurses
     endwin();
-    return -1; // 选择失败
+    return -1; // SELECTION FAILED
 }
 
 
 void customize(int &width, int &height, int &timelimit, int &bomb, string username) {
-    // 初始化ncurses
+    
+    // INITIALIZE NCURSES
     initscr();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
 
-    // 获取窗口尺寸
-    int maxRows, maxCols;
-    getmaxyx(stdscr, maxRows, maxCols);
-
-    // 定义输入框的位置
-    int startX = maxCols / 2 - 15;
-    int startY = maxRows / 2 - 2;
-
-    // 定义输入框的名称
+    // NAME OF INPUT BOX
     string names[4] = {"Width(5-30)", "Height(5-30)", "Time Limit(>0)", "Number of Bomb(>=0)"};
 
-    // 初始化输入框的值
+    // VALUE OF INPUT BOX
     string values[4];
 
-    // 当前选中的输入框索引
     int currentField = 0;
     bool invalid = false;
 
@@ -131,10 +123,10 @@ void customize(int &width, int &height, int &timelimit, int &bomb, string userna
         if (invalid)
             mvprintw(16, 2, "**Invalid Input!");
 
-        // 打印输入框和值
+        // PRINT INPUT BOX
         for (int i = 0; i < 4; i++) {
             if (i == currentField) {
-                attron(A_REVERSE);  // 高亮当前选中的输入框
+                attron(A_REVERSE);  // CURRENT SELECTION -> DIFFERENT COLOR
             }
 
             mvprintw(11 + i, 2, "· %s: %s", names[i].c_str(), values[i].c_str());
@@ -144,19 +136,17 @@ void customize(int &width, int &height, int &timelimit, int &bomb, string userna
             }
         }
 
-        // 刷新屏幕
         refresh();
 
-        // 获取用户输入
+        // USER INPUT
         int key = getch();
 
-        // 处理用户输入
         if (key == KEY_UP) {
             if (invalid == false)
-                currentField = (currentField - 1 + 4) % 4;  // 上移当前选中的输入框
+                currentField = (currentField - 1 + 4) % 4;  // MOVE THE CURRENT SELECTION UP
         } else if (key == KEY_DOWN) {
             if (invalid == false )
-                currentField = (currentField + 1) % 4;  // 下移当前选中的输入框
+                currentField = (currentField + 1) % 4;  // MOVE THE CURRENT SELECTION DOWN
         } else if (key == '\n') {
             bool allFilled = true; // CHECK IF EMPTY
             for (const auto& value : values)
@@ -164,14 +154,13 @@ void customize(int &width, int &height, int &timelimit, int &bomb, string userna
                     allFilled = false;
             invalid = false;
             for (const auto& value : values[currentField])
-                if (value < '0' || value > '9'){
+                if (value < '0' || value > '9')
                     invalid = true;
-                }
             if (invalid)
-                continue;
+                continue; // INVALID INPUT
             if (values[currentField].empty()){
                 currentField = (currentField + 1) % 4;
-                continue;
+                continue; // SOME BOX ARE NOT FILLED
             }
             int value = stoi(values[currentField]); // CHECK VALID INPUT
             if ((currentField == 0 || currentField == 1) && (value < 5 || value > 30))
@@ -185,19 +174,34 @@ void customize(int &width, int &height, int &timelimit, int &bomb, string userna
                 height = stoi(values[1]);
                 timelimit = stoi(values[2]);
                 bomb = stoi(values[3]);
-                return;
+                return; // RETURN INPUT (PASS BY REFERENCE)
             }
             else{
                 currentField = (currentField + 1) % 4;
             }
         } else if (key == KEY_BACKSPACE || key == 127) {
-            // 处理退格键
+            // HANDLE BACKSPACE KEY
             if (!values[currentField].empty()) {
-                values[currentField].pop_back();  // 删除输入框中的最后一个字符
+                values[currentField].pop_back();  // DELETE THE LAST CHAR OF THE BOX
             }
+            invalid = false;
+            if (values[currentField].empty())
+                continue; // NO VALUE
+            for (const auto& value : values[currentField])
+                if (value < '0' || value > '9')
+                    invalid = true;
+            if (invalid)
+                continue; // CONTAIN INVALID INPUT
+            int value = stoi(values[currentField]); // CHECK VALID INPUT
+            if ((currentField == 0 || currentField == 1) && (value < 5 || value > 30))
+                invalid = true;
+            else if ((currentField == 2) && (value <= 0))
+                invalid = true;
+            else if ((currentField == 3) && (value < 0))
+                invalid = true;
         } else {
-            // 处理数值输入
-            values[currentField] += key;  // 将输入字符添加到当前选中的输入框中
+            // HANDLE VALUE INPUT
+            values[currentField] += key;  // ADD INPUT TO THE VALUE
             invalid = false;
             if (key < '0' || key > '9'){
                 invalid = true;
